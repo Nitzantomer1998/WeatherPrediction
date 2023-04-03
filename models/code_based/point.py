97d84b67e6d94b4faada3f02eaddcb0bcc9357aa
nitzan <nitzantomer1998@gmail.com>
2023-04-03 08:45:08 +0300

    Adjusting notes

--- models/code_based/point.py
+++ models/code_based/point.py
@@ -1,4 +1,11 @@
 class Point:
     def __init__(self, dimensions: tuple, classifier: str) -> None:
+        """
+        Initializes a Point object with given dimensions and a classifier.
+
+        Args:
+            dimensions (tuple): The tuple of values representing the point's position in n-dimensional space.
+            classifier (str): The classification label of the point.
+        """
         self.dimensions = dimensions
         self.classifier = classifier
